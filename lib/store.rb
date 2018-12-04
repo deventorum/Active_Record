177d8d41b5f1df4cc25b42cfa1e55e611a8ed1ef
deventorum <deventorum@gmail.com>
2018-12-04 20:56:46 +0000

    Exercise 9 done

--- lib/store.rb
+++ lib/store.rb
@@ -6,10 +6,21 @@ class Store < ActiveRecord::Base
                                              message: 'should not be a negative number' }
   validate :at_least_one_apparel
 
+  before_destroy :check_data
+
   def at_least_one_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, 'must have at least one type of apparel')
       errors.add(:womens_apparel, 'must have at least one type of apparel')
+    end
+  end
+
+  private
+
+  def check_data
+    if Employee.where("store_id = #{id}").count != 0
+      errors.add :base, 'Cannot destroy store with employees'
+      throw(:abort)
     end
   end
 end
