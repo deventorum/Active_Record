24b4c0193b86fccc4d231098d5b8070b61a3f234
deventorum <deventorum@gmail.com>
2018-12-04 14:41:04 +0000

    Exercise 2 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,6 +6,6 @@ puts '----------'
 # Your code goes below here ...
 store = Store.create(name: 'Burnaby', mens_apparel: true, womens_apparel: true, annual_revenue: 300_000)
 store = Store.create(name: 'Richmond', mens_apparel: false, womens_apparel: true, annual_revenue: 1_260_000)
-store = Store.create(name: 'Gastown', mens_apparel: true, womens_apparel: false, annual_revenue: 1_260_000)
+store = Store.create(name: 'Gastown', mens_apparel: true, womens_apparel: false, annual_revenue: 190_000)
 
 puts Store.count
